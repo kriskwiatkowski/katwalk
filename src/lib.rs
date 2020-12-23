c3b50f6464fc40937db513e3816bd5341eae7235
Kris Kwiatkowski <contact@amongbytes.com>
2020-12-23 09:59:20 +0000

    shake: add fixed len shake

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,7 @@ pub mod reader {
 	#[derive(Debug, Default)]
 	pub struct Xof {
 		pub count: usize,
+		pub len: usize,
 		pub outputlen: usize,
 		pub msg: Vec<u8>,
 		pub output: Vec<u8>,
@@ -156,6 +157,7 @@ pub mod reader {
 				"COUNT" => self.count = super::to_uint(v),
 				"Outputlen" => self.outputlen = super::to_uint(v),
 				"Msg" => self.msg = super::to_u8arr(v),
+				"Len" => self.len = super::to_uint(v),
 				"Output" => {
 					self.output = super::to_u8arr(v);
 					return ReadResult::ReadDone;
@@ -284,7 +286,7 @@ MD = 6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7";
 	}
 
 	#[test]
-	fn test_xof_parsing() {
+	fn test_xof_variable_len_parsing() {
 		let ex = "
 #  CAVS 19.0
 #  SHAKE256 VariableOut information for SHAKE3AllBitsGT
@@ -314,6 +316,38 @@ COUNT = 1
 			assert_eq!(el.xof.msg[0..5], [0x37, 0x43, 0x34, 0x97, 0x79]);
 			assert_eq!(el.xof.output.len(), el.xof.outputlen);
 			assert_eq!(el.xof.output, [0xEA, 0x93, 0x0A]);
+			count+=1;
+		}
+		assert_eq!(count, 1);
+	}
+
+	#[test]
+	fn test_xof_fixed_len_parsing() {
+		let ex = "
+#  CAVS 19.0
+#  SHAKE128 LongMsg information for SHAKE3AllBytesGT
+#  SHAKE128 tests are configured for BYTE oriented implementations
+#  Length values represented in bits
+#  Generated on Thu Jan 28 14:46:45 2016
+
+[Outputlen = 128]
+
+Len = 2696
+Msg = a6fe00064257aa318b621c5eb311d32bb8004c2fa1a969d205d71762cc5d2e633907992629d1b69d9557ff6d5e8deb454ab00f6e497c89a4fea09e257a6fa2074bd818ceb5981b3e3faefd6e720f2d1edd9c5e4a5c51e5009abf636ed5bca53fe159c8287014a1bd904f5c8a7501625f79ac81eb618f478ce21cae6664acffb30572f059e1ad0fc2912264e8f1ca52af26c8bf78e09d75f3dd9fc734afa8770abe0bd78c90cc2ff448105fb16dd2c5b7edd8611a62e537db9331f5023e16d6ec150cc6e706d7c7fcbfff930c7281831fd5c4aff86ece57ed0db882f59a5fe403105d0592ca38a081fed84922873f538ee774f13b8cc09bd0521db4374aec69f4bae6dcb66455822c0b84c91a3474ffac2ad06f0a4423cd2c6a49d4f0d6242d6a1890937b5d9835a5f0ea5b1d01884d22a6c1718e1f60b3ab5e232947c76ef70b344171083c688093b5f1475377e3069863
+Output = 3109d9472ca436e805c6b3db2251a9bc
+
+";
+
+		let r = KatReader::new(
+			std::io::BufReader::new(Cursor::new(ex)),
+			AlgType::AlgXof, 1);
+
+		let mut count = 0;
+		for el in r {
+			assert_eq!(el.xof.msg[0..5], [0xA6, 0xFE, 0x00, 0x06, 0x42]);
+			assert_eq!(el.xof.output.len(), 128/8);
+			assert_eq!(el.xof.output[0..3], [0x31, 0x09, 0xD9]);
+			assert_eq!(el.xof.len, 2696);
 			count+=1;
 		}
 		assert_eq!(count, 1);
