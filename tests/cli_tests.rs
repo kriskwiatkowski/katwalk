e353296baca89ede1cf7c233e0dcc2e488341688
Kris Kwiatkowski <contact@amongbytes.com>
2026-09-19 17:15:14 +0100

    [test] Test uses wrong version

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -24,7 +24,7 @@ fn test_cli_version() {
 
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("0.0.1"));
+        .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")));
 }
 
 #[test]
